0a679760d8ddcf61400b79a4bc6b0b90a9b60f97
Thomas Goureau <thomas.goureau@gmail.com>
2016-03-18 10:31:09 +0100

    customsempahore

---
+++ src/main/CustomSemaphore.java
@@ -0,0 +1,25 @@
+package main;
+
+import interfaces.SemaphoreInterface;
+
+public class CustomSemaphore implements SemaphoreInterface {
+
+	@Override
+	public void up() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void down() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int releaseAll() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
